a204e1631f7e00ad407e3bdc5d2271e21c0b1d16
annaermoshkina <118661438+annaermoshkina@users.noreply.github.com>
2022-12-07 19:05:40 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+if (powG>resultMax)
+{
+    int resultMax=powF;
+    int resultMin=powG;
+}
+int[] result=new int[resultMax];
+for (int i=0; i<resultMin; i++)
+{
+    result[i]=f[i]-g[i];
+}
+for (int i=resultMin; i<resultMax; i++)
+{
+    if (resultMax=powG) result[i]=g[i]*(-1);
+    else result[i]=f[i];
+}
